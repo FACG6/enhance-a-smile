1e14771eba47ab33a0f0f12dd9318409f6ec5b52
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-19 15:35:36 +0200

    solve conflict

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,8 +8,8 @@ router.get('/request',requestForYou.get)
 router.get('/donate', donate);
 
 
-const { donate } = require('./main/donat')
 
-router.get('/donate', donate);
+
+
 
 module.exports = { router };
