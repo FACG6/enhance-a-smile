050093c3220a6b3264956381188a289ccfaebb04
aminalakhsham <aminking140@gmail.com>
2019-03-19 15:21:32 +0200

    add routes / for method get

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { getHomePage } = require('./user/getHomePage');
 
 const router = express.Router();
+
+router.get('/', getHomePage);
 
 module.exports = { router };
