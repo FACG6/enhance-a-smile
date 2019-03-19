114be07a965de3aa5687e923f0eab2c557f271ac
aminalakhsham <aminking140@gmail.com>
2019-03-19 11:14:34 +0200

    require all package and make it as midlware and add hbr engin

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { join } = require('path');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const { router } = require('./controllers/index');
+require('dotenv').config();
+
+const app = express();
+app.disable('x-powered-by');
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
+app.set('port', process.env.PORT || 7425);
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  layoutsDir: join(__dirname, 'views', 'layouts'),
+  partialsDir: join(__dirname, 'views', 'partials'),
+  defaultLayout: 'main',
+}));
+app.use(router);
+
+module.exports = app;
