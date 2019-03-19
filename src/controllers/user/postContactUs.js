f48ce0e1cee0d807a10321e67132c744e21c4929
aminalakhsham <aminking140@gmail.com>
2019-03-19 19:50:28 +0200

    make validation for contact us data

--- src/controllers/user/postContactUs.js
+++ src/controllers/user/postContactUs.js
@@ -1,4 +1,18 @@
+const joi = require('joi');
+
 exports.postContactUs = (request, response) => {
   console.log(request.body);
-  response.send({ msg: 'done' });
+  const schema = joi.object().keys({
+    email: joi.string().email({ minDomainAtoms: 2 }),
+    fullName: joi.string().alphanum().max(30).required(),
+    message: joi.string().alphanum().required(),
+  });
+  const { email, fullName, message } = request.body;
+  const result = joi.validate({ email, fullName, message }, schema);
+  console.log(result);
+  if (result.error === null) {
+    response.send({ msg: 'done' });
+  } else {
+    response.send({ msg: 'inputs not validate' });
+  }
 };
