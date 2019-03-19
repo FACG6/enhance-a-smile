07b979ebf8a9c5716b1f64bf1b1ae3224ef18969
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-19 15:28:51 +0200

    add package-lock file

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const requestForYou=require('./main/request')
 
 const router = express.Router();
+router.get('/request',requestForYou.get)
 
 module.exports = { router };
