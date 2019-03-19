4446da2a377b3d9b2db8e18386404973f56f8f12
aminalakhsham <aminking140@gmail.com>
2019-03-19 19:14:57 +0200

    handle roustes /register and /contact-us with method post

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { getHomePage } = require('./user/getHomePage');
 const { donate } = require('./main/donat');
+const { postContactUs } = require('./user/postContactUs');
+const { postRegisterEmail } = require('./user/postRegisterEmail');
 
 const router = express.Router();
 
 router.get('/', getHomePage);
-
+router.post('/contact-us', postContactUs);
+router.post('/register', postRegisterEmail);
 router.get('/donate', donate);
 
 module.exports = { router };
