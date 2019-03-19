146477da9d4d8e8403da438fb308abcdb6eb48a3
aminalakhsham <aminking140@gmail.com>
2019-03-19 22:19:03 +0200

    add fuction for queryselctor

--- public/js/user/home.js
+++ public/js/user/home.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const { formContactUs, registerEmail } = querySelectors(['formContactUs', 'registerEmail'], ['.footer--contactUs-form', '.footer--form']);
 
-=======
-const formContactUs = document.querySelector('.footer--contactUs-form');
-const registerEmail = document.querySelector('.footer--form');
-const popup = document.querySelector('.popup');
-const poppUpDone = document.querySelector('.popup--content-Done');
->>>>>>> 79886b2fc59526b1a48c17495654b7551f2f3232
 
 formContactUs.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -19,9 +12,6 @@ formContactUs.addEventListener('submit', (e) => {
     method: 'POST',
     body: JSON.stringify(dataContactUs),
     headers: { 'Content-Type': 'application/json' },
-<<<<<<< HEAD
-  });
-=======
   })
     .then(res => res.json())
     .then((res) => {
@@ -29,7 +19,6 @@ formContactUs.addEventListener('submit', (e) => {
         popup.style.display = 'flex';
       }
     });
->>>>>>> 79886b2fc59526b1a48c17495654b7551f2f3232
 });
 
 registerEmail.addEventListener('submit', (e) => {
@@ -43,9 +32,6 @@ registerEmail.addEventListener('submit', (e) => {
     method: 'POST',
     body: JSON.stringify(dataregisterEmail),
     headers: { 'Content-Type': 'application/json' },
-<<<<<<< HEAD
-  });
-=======
   })
     .then(res => res.json())
     .then((res) => {
@@ -58,5 +44,4 @@ registerEmail.addEventListener('submit', (e) => {
 poppUpDone.addEventListener('click', (e) => {
   e.preventDefault();
   popup.style.display = 'none';
->>>>>>> 79886b2fc59526b1a48c17495654b7551f2f3232
 });
