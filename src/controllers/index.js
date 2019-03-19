ce4754eb4db9e83d656706b8a6d2f4caea02e7d7
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-19 15:31:17 +0200

    solve  conflict

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const requestForYou=require('./main/request')
+const { donate } = require('./main/donat')
 
 const router = express.Router();
 router.get('/request',requestForYou.get)
+
+router.get('/donate', donate);
+
 
 module.exports = { router };
