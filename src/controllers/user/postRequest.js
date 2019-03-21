c3312a4daeadf4eb658fe70ffe3107ffa58b45ba
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-21 10:30:45 +0200

    add query postRequest to post all information to database #53

--- src/controllers/user/postRequest.js
+++ src/controllers/user/postRequest.js
@@ -1,4 +1,9 @@
+const { insertRequest } = require('../../database/queries/user/addRequest');
+
 exports.postRequest = (request, response) => {
   const requestInformation = request.body;
+  insertRequest(requestInformation).then(re => console.log(re)).catch(re => console.log(re));
+
+
   response.send({ msg: 'Your request added sucsesfully' });
 };
