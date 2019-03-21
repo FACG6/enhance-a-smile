e006c3c88b038f34ec7e193557f8a50d550d2d08
aminalakhsham <aminking140@gmail.com>
2019-03-21 16:18:07 +0200

    fix conflict

--- src/controllers/user/postContactUs.js
+++ src/controllers/user/postContactUs.js
@@ -1,4 +1,5 @@
 const joi = require('joi');
+const insertQuery = require('./../../database/queries/user/insertQuery');
 
 exports.postContactUs = (request, response) => {
   const schema = joi.object().keys({
@@ -14,9 +15,16 @@ exports.postContactUs = (request, response) => {
       .required(),
   });
   const { email, fullName, message } = request.body;
+  const data = {
+    email,
+    fullName,
+    message,
+  };
   const result = joi.validate({ email, fullName, message }, schema);
   if (result.error === null) {
-    response.send({ msg: 'done' });
+    insertQuery('registers', data).then(() => {
+      response.send({ msg: 'done' });
+    });
   } else {
     response.send({ msg: 'inputs not validate' });
   }
