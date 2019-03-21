a524f9b32e98343ce5dd9327cb60f71f6de06018
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-03-21 14:31:53 +0200

    add back-end validation

--- src/controllers/user/helpOthers.js
+++ src/controllers/user/helpOthers.js
@@ -1,4 +1,5 @@
 const { join } = require('path');
+const joi = require('joi');
 const insertHelpOther = require('./../../database/queries/user/addHelpOthers.js');
 
 exports.get = (req, res) => {
@@ -9,11 +10,52 @@ exports.get = (req, res) => {
 };
 
 exports.post = (req, res) => {
-  insertHelpOther(req.body)
-    .then(() => {
-      res.status(200).end();
-    })
-    .catch((err) => {
-      res.status(500).end();
+  const schema = joi.object().keys({
+    'personal-details': {
+      fullName: joi
+        .string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+      phoneNumber: joi
+        .string()
+        .regex(/^[0-9]{10}$/)
+        .length(10)
+        .required(),
+      email: joi.string().email({ minDomainAtoms: 2 }),
+    },
+    'request-details': {
+      numOfPeople: joi
+        .number()
+        .min(1)
+        .max(5),
+      phoneNumber: joi
+        .string()
+        .regex(/^[0-9]{10}$/)
+        .length(10)
+        .required(),
+      location: joi
+        .string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+      description: join.string().min(1),
+    },
+  });
+  const valid = joi.validate(req.body, schema);
+  if (valid.error) {
+    res.status(400).send({
+      msg: valid.error.details[0].message,
     });
+  } else {
+    insertHelpOther(req.body)
+      .then(() => {
+        res.status(200).end();
+      })
+      .catch((err) => {
+        res.status(500).end();
+      });
+  }
 };
