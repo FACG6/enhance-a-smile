c0e4061174a857d45ca4879ea60fd2750359837a
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-03-21 09:44:22 +0200

    make the insert query function

---
+++ src/database/queries/user/insertDonate.js
@@ -0,0 +1,15 @@
+const client = require('../../db_connection');
+
+const insertDonation = donateInfo => new Promise((resolve, reject) => {
+  client.connect((error) => {
+    if (error) reject(error);
+    const database = client.db('enhance-a-smile-db');
+    database.collection('donates').insertOne(donateInfo);
+    resolve({ msg: 'donate added sucssfully' });
+    client.close();
+  });
+});
+
+module.exports = {
+  insertDonation,
+};
