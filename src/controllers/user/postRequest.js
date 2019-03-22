2919f65bf70ced6bbeeed327df2d83331e3a5e8b
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-22 23:06:35 +0200

    add test for insertQuery by adding data to requests collection in database

--- src/controllers/user/postRequest.js
+++ src/controllers/user/postRequest.js
@@ -1,8 +1,8 @@
-const { insertRequest } = require('../../database/queries/user/addRequest');
+const insertQuery = require('../../database/queries/user/insertQuery');
 
 exports.postRequest = (request, response) => {
   const requestInformation = request.body;
-  insertRequest(requestInformation).then(re => console.log(re)).catch(re => console.log(re));
+  insertQuery('requests', requestInformation).then(re => console.log(re)).catch(re => console.log(re));
 
 
   response.send({ msg: 'Your request added sucsesfully' });
