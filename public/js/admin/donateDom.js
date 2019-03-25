dc807577c72393d965c3af396fc95f2a25c0de19
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-03-25 19:47:08 +0200

    add the evvnt listener to the btn

--- public/js/admin/donateDom.js
+++ public/js/admin/donateDom.js
@@ -1,16 +1,25 @@
-const infoBtn = document.querySelector('.tap--personal');
-const donateBtn = document.querySelector('.tap--donation');
-const donateDiv = document.querySelector('.cards--info-donation');
-const infoDiv = document.querySelector('.cards--info-personal');
+const infoBtn = document.querySelectorAll('.tap--personal');
+const donateBtn = document.querySelectorAll('.tap--donation');
+const donateDiv = document.querySelectorAll('.cards--info-donation');
+const infoDiv = document.querySelectorAll('.cards--info-personal');
 
-infoBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  infoDiv.classList.remove('hide');
-  donateDiv.classList.add('hide');
+const infoBtnArray = Array.from(infoBtn);
+const donateBtnArray = Array.from(donateBtn);
+const infoDivArray = Array.from(infoDiv);
+const donateDivArray = Array.from(donateDiv);
+
+infoBtnArray.forEach((element, index) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    infoDivArray[index].classList.remove('hide');
+    donateDivArray[index].classList.add('hide');
+  });
 });
 
-donateBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  infoDiv.classList.add('hide');
-  donateDiv.classList.remove('hide');
+donateBtnArray.forEach((element, index) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    infoDivArray[index].classList.add('hide');
+    donateDivArray[index].classList.remove('hide');
+  });
 });
