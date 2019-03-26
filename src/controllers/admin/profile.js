364506d439bc8d791fc776ef042900a27111a743
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-03-26 15:01:01 +0200

    write the style for admin's sidebar and it's dom

--- src/controllers/admin/profile.js
+++ src/controllers/admin/profile.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 exports.get = (req, res) => {
   res.render(join('admin', 'profile.hbs'), {
     css: [join('partials', 'adminNav'), join('partials', 'adminSidebar')],
+    js: ['domUyils', join('admin', 'sidebar')],
     layout: 'admin',
   });
 };
