cb8a223048f5ce39a7d8cd77b4c8627a5448fb88
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-26 18:50:44 +0200

    add handling of profile page

--- src/controllers/admin/profile.js
+++ src/controllers/admin/profile.js
@@ -1,7 +1,15 @@
 const { join } = require('path');
+const getQuery = require('../../database/queries/admin/getQuery');
 
-exports.get = (req, res) => {
-  res.render(join('admin', 'profile.hbs'), {
-    layout: 'admin',
+exports.get = (request, response) => {
+  const fullName = request.token;
+  getQuery('admins', { full_name: 'Ahmed Abdellatif' }).then((res) => {
+    response.render(join('admin', 'profile'), {
+      layout: 'admin',
+      result: res,
+      css: [join('admin', 'profileAdmin')],
+    });
+  }).catch((error) => {
+    response.send(error);
   });
 };
