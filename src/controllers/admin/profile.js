42edde6f898082f8325c97e6cec7e4a030c4d4f5
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-03-26 14:05:56 +0200

    admin style

--- src/controllers/admin/profile.js
+++ src/controllers/admin/profile.js
@@ -2,6 +2,7 @@ const { join } = require('path');
 
 exports.get = (req, res) => {
   res.render(join('admin', 'profile.hbs'), {
+    css: [join('partials', 'adminNav'), join('partials', 'adminSidebar')],
     layout: 'admin',
   });
 };
