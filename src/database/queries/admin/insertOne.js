afbe2d230e1ae0f12e73773efd6ca7916e57075e
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-03-26 20:25:56 +0200

    remove commented code

--- src/database/queries/admin/insertOne.js
+++ src/database/queries/admin/insertOne.js
@@ -21,26 +21,3 @@ module.exports = (collectionName, id, obj) => new Promise((resolve, reject) => {
       });
   });
 });
-
-
-
-// client.connect((error) => {
-//   if (error) reject(error);
-//   const database = client.db('enhance-a-smile-db');
-//   database
-//     .collection(collectionName)
-//     .findOneAndUpdate({
-//       _id: ObjectId(id),
-//     }, {
-//       $set: obj,
-//     }, {
-//       returnNewDocument: true,
-//     })
-//     .then((res) => {
-//       resolve(res);
-//     })
-//     .catch((err) => {
-//       client.close();
-//       reject(err);
-//     });
-// });
