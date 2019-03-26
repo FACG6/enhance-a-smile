8c2a0309d4d2123f64e11bd5dfd1bec1853c67a5
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-03-26 18:50:17 +0200

    edit the query to avoide the error in the databae

--- src/database/queries/admin/insertOne.js
+++ src/database/queries/admin/insertOne.js
@@ -1,11 +1,10 @@
-const client = require('./../../db_connection');
+const connection = require('../../db_connection');
 const { ObjectId } = require('mongodb');
 
 module.exports = (collectionName, id, obj) => new Promise((resolve, reject) => {
-  client.connect((error) => {
-    if (error) reject(error);
-    const database = client.db('enhance-a-smile-db');
-    database
+  connection().then((dbs) => {
+    const dataBase = dbs.production;
+    dataBase
       .collection(collectionName)
       .findOneAndUpdate({
         _id: ObjectId(id),
@@ -18,8 +17,30 @@ module.exports = (collectionName, id, obj) => new Promise((resolve, reject) => {
         resolve(res);
       })
       .catch((err) => {
-        client.close();
         reject(err);
       });
   });
 });
+
+
+
+// client.connect((error) => {
+//   if (error) reject(error);
+//   const database = client.db('enhance-a-smile-db');
+//   database
+//     .collection(collectionName)
+//     .findOneAndUpdate({
+//       _id: ObjectId(id),
+//     }, {
+//       $set: obj,
+//     }, {
+//       returnNewDocument: true,
+//     })
+//     .then((res) => {
+//       resolve(res);
+//     })
+//     .catch((err) => {
+//       client.close();
+//       reject(err);
+//     });
+// });
