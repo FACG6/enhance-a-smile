23bdde842314ae8adc5ea73d18ac7215cfbb0f41
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-03-26 12:23:27 +0200

    fix bugs in the getQuery function

--- src/database/queries/admin/getQuery.js
+++ src/database/queries/admin/getQuery.js
@@ -1,18 +1,16 @@
 const connection = require('../../db_connection');
 
 module.exports = (collectionName, obj) => new Promise((resolve, reject) => {
-  connection((dbs) => {
+  connection().then((dbs) => {
     const dataBase = dbs.production;
     dataBase
       .collection(collectionName)
       .find(obj)
       .toArray()
       .then((res) => {
-        console.log('saddddddwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwdddddd');
         resolve(res);
       })
       .catch((err) => {
-        console.log('szzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzddd');
         reject(err);
       });
   });
