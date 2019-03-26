3c3a84129779f6341fc977b0f80109b3142925f8
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-26 23:09:10 +0200

    handlig admin profile page

--- src/controllers/admin/profile.js
+++ src/controllers/admin/profile.js
@@ -3,14 +3,22 @@ const getQuery = require('../../database/queries/admin/getQuery');
 
 exports.get = (request, response) => {
   const fullName = request.token;
-  getQuery('admins', { full_name: 'Ahmed Abdellatif' }).then((res) => {
-    response.render(join('admin', 'profile'), {
-      layout: 'admin',
-      result: res,
-      css: [join('admin', 'profileAdmin'), join('partials', 'adminNav'), join('partials', 'adminSidebar')],
-      js: ['domUyils', join('admin', 'sidebar')],
+  let totalDonation = 0;
+  getQuery('admins', { full_name: 'Ahmed Abdellatif' })
+    .then((res) => {
+      getQuery('donates', {}).then((totalDonations) => {
+        if (Object.keys(res).length !== 0) {
+          totalDonation = Object.keys(totalDonations).length;
+        }
+        response.render(join('admin', 'profile'), {
+          layout: 'admin',
+          result: res,
+          totalDonations: totalDonation,
+          css: [join('admin', 'profile'), join('partials', 'adminNav'), join('partials', 'adminSidebar')],
+          js: ['domUyils', join('admin', 'sidebar')],
+        });
+      });
+    }).catch((error) => {
+      response.send(error);
     });
-  }).catch((error) => {
-    response.send(error);
-  });
 };
