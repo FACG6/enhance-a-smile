840ed87e8278c2d0316393baade86a788fc23824
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-27 00:56:33 +0200

    add staucture of admin profile page Relates #67

--- src/controllers/admin/profile.js
+++ src/controllers/admin/profile.js
@@ -2,9 +2,11 @@ const { join } = require('path');
 const getQuery = require('../../database/queries/admin/getQuery');
 
 exports.get = (request, response) => {
-  const fullName = request.token;
+  const fullName = request.cookies.name;
   let totalDonation = 0;
-  getQuery('admins', { full_name: 'Ahmed Abdellatif' })
+  getQuery('admins', {
+    full_name: fullName,
+  })
     .then((res) => {
       getQuery('donates', {}).then((totalDonations) => {
         if (Object.keys(res).length !== 0) {
@@ -15,7 +17,7 @@ exports.get = (request, response) => {
           result: res,
           totalDonations: totalDonation,
           css: [join('admin', 'profile'), join('partials', 'adminNav'), join('partials', 'adminSidebar')],
-          js: ['domUyils', join('admin', 'sidebar')],
+          js: ['domUyils', join('admin', 'profile'), join('admin', 'sidebar')],
         });
       });
     }).catch((error) => {
