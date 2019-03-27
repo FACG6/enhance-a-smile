b459ae308d946080af1cdb882868ca7ab6f0f42d
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-27 07:00:50 +0200

    solve problem in test od getQuery

--- test/getQuery.test.js
+++ test/getQuery.test.js
@@ -2,9 +2,9 @@ const test = require('tape');
 const getQuery = require('../src/database/queries/admin/getQuery');
 
 test('test for getQuery to get data from database', (t) => {
-  const expected = 'Ahmed Abdellatif';
+  const expected = 'hamod';
 
-  getQuery('admins', { full_name: 'Ahmed Abdellatif' }).then((res) => {
+  getQuery('admins', { full_name: 'hamod' }).then((res) => {
     t.equal(expected, res[0].full_name, 'test of query of get data is done');
     t.end();
   });
