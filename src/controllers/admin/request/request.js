b1143eca6cf2f3ba10c8ecbb16e2bd0c920220ee
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-03-27 11:08:04 +0200

    handel request admin page

--- src/controllers/admin/request/request.js
+++ src/controllers/admin/request/request.js
@@ -1,9 +1,25 @@
 const { join } = require('path');
+const getQuery = require('../../../database/queries/admin/getQuery');
 
 exports.get = (request, response) => {
-  response.render(join('admin', 'requests'), {
-    css: [join('partials', 'adminSidebar'), join('partials', 'homeNav'), join('admin', 'request')],
-    js: ['domUyils', join('admin', 'requestDom')],
-    layout: 'admin',
-  });
+  let totalrequests = 0;
+  getQuery('requests', {
+    status: 'new',
+  })
+    .then((res) => {
+      getQuery('requests', { }).then((totalRquests) => {
+        if (Object.keys(totalRquests).length !== 0) {
+          totalrequests = Object.keys(totalRquests).length;
+        }
+        response.render(join('admin', 'requests'), {
+          layout: 'admin',
+          result: res,
+          totalrequests,
+          css: [join('admin', 'request'), join('partials', 'adminNav'), join('partials', 'adminSidebar')],
+          js: ['domUyils', join('admin', 'requestDom'), join('admin', 'sidebar')],
+        });
+      });
+    }).catch((error) => {
+      response.send(error);
+    });
 };
